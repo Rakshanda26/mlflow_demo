dacded1a632050cce7e5ad6e20a6799fc0b221b6
Rakshanda@26 <rakshandamm123@gmail.com>
2024-12-03 22:00:42 +0530

    completed

---
+++ main.py
@@ -0,0 +1,26 @@
+import mlflow
+
+def calculator(a,b,operation=None):
+    if operation == "add":
+        return (a+b)
+
+    if operation == "sub":
+        return (a-b)
+
+    if operation == "mul":
+        return (a*b)
+
+    if operation == "div":
+        return (a/b)
+
+
+if __name__ == "__main__":
+    a,b,opt=1084,3140,"mul"
+    with mlflow.start_run():
+        result = calculator(a, b, opt)
+        mlflow.log_param("a", a)
+        mlflow.log_param("b", b)
+        mlflow.log_param("opt", opt )
+        mlflow.log_param("result" ,result)
+        print(f"my result is {result}")
+
